2c651fc81da92760a252fc76f36494009b7ac970
dipboruah <dipboruah@outlook.com>
2023-04-03 01:42:31 +0530

    cors configuration

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ connectDB();
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
